b7d5d569fe3084c810359b17b47bd23a29a206b4
Israel Quishpe <israel.quishpe@outlook.es>
2020-05-14 13:40:35 -0500

    Jueves

--- proyecto1/src/app/app.module.ts
+++ proyecto1/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MiPrimerComponenteComponent } from './mi-primer-componente/mi-primer-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MiPrimerComponenteComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
