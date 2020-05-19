0791bce4ec7e11250ebc1a4b2091c1e26467f706
Israel Quishpe <israel.quishpe@outlook.es>
2020-05-18 22:10:38 -0500

    DeberLunes18

--- proyecto-angular/src/app/app.component.ts
+++ proyecto-angular/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyecto-angular';
+  esconderHola = false;
+  arregloNuemros = [{ nombre: 'Israel' }, { nombre: 'orlando' }];
+
+  eventoClick() {
+    this.esconderHola = true;
+  }
 }
