a0802640d32e0ee47ce1aade1d32a44b8e9c4009
Israel Quishpe <israel.quishpe@outlook.es>
2020-05-19 18:00:34 -0500

    Martes19

---
+++ rutas-angular/src/app/app.routes.ts
@@ -0,0 +1,25 @@
+
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./inicio/home/home.component";
+import { NgModule } from "@angular/core";
+
+const rutas: Routes = [
+    {
+        path: 'inicio',
+        component: HomeComponent
+    },
+    {
+
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(rutas)],
+    providers: [],
+    declarations: [],
+    exports: [RouterModule]
+})
+
+export class AppRoutesComponent {
+
+}
